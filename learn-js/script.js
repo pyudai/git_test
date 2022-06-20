d12f16ab8feda51bcbee2d11a503add9bd01484a
pyudai <premyuda321@gmail.com>
2022-06-21 01:00:45 +0700

    learn js fundamentals part 1

---
+++ learn-js/script.js
@@ -0,0 +1,11 @@
+/* Numbers */
+console.log(23 + 97);
+
+console.log(1 + 28 + 21 + 32 + 5 + 23);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
