84430bb1ffb6c43626a737733ab394e55c11e84c
pyudai <premyuda321@gmail.com>
2022-07-12 01:12:41 +0700

    create prettier UI

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -12,16 +12,20 @@ const getGameElementIndex = (str) =>
 
 // one round of game -> return the person who won the game
 function playRound(playerSelection, computerSelection) {
-  playerSelection = getGameElementIndex(playerSelection);
+  playerSelection = getGameElementIndex(playerSelection.trim());
+  const div = document.querySelector("#round-result");
 
   if (playerSelection < 0) {
     //if user not input the gameElement
     console.log("It isn't a game element. Please enter again");
-    return null;
+    div.removeAttribute("class");
+    return { result: null, str: "It isn't a game element. Please enter again" };
   }
 
   if (playerSelection === computerSelection) {
     // draw
+    div.removeAttribute("class");
+    div.setAttribute("class", "draw");
     return { result: "draw", str: `Draw! ${gameElements[playerSelection]}` };
   } else if (
     // player win
@@ -29,32 +33,36 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === 1 && computerSelection === 0) || // player : Paper     , com : Rock
     (playerSelection === 0 && computerSelection === 2) // player : Rock      , com : Scissors
   ) {
+    div.removeAttribute("class");
+    div.setAttribute("class", "win");
     return {
       result: "player",
       str: `You win! ${gameElements[playerSelection]} beats ${gameElements[computerSelection]}`,
     };
   } else {
     // com win
+    div.removeAttribute("class");
+    div.setAttribute("class", "lose");
     return {
       result: "computer",
       str: `You Lose! ${gameElements[computerSelection]} beats ${gameElements[playerSelection]}`,
     };
   }
 }
+
 let scores = { player: 0, computer: 0 };
 
 function playerSelect(e) {
   const { result, str } = playRound(e.textContent, computerPlay());
-  const div = document.querySelector("#resultStr");
-  const scoreDiv = document.querySelectorAll(".score");
+  const div = document.querySelector("#round-result");
+  const scoreDiv = document.querySelectorAll(".score > div:nth-child(2)");
   div.textContent = str;
 
   if (result && !result.includes("draw")) scores[result] += 1;
   scoreDiv.forEach((score) => (score.textContent = scores[score.id]));
-  
+
   // check if one player reaches 5 points
   if (scores.computer === 5 || scores.player === 5) {
-    console.log("end Game");
     document.querySelectorAll("button").forEach((btn) => (btn.disabled = true));
     const restart = document.createElement("button");
     restart.id = "restart";
@@ -65,34 +73,14 @@ function playerSelect(e) {
       document
         .querySelectorAll("button")
         .forEach((btn) => (btn.disabled = false));
+      document.querySelector("#round-result").removeAttribute("class");
       document
-        .querySelector("body")
+        .querySelector("#round-result")
         .removeChild(document.querySelector("#restart"));
+      document.querySelector("#round-result").textContent =
+        "Choose Your Weapon";
     });
-    document.querySelector("body").appendChild(restart);
+    document.querySelector("#round-result").textContent = "";
+    document.querySelector("#round-result").appendChild(restart);
   }
 }
-
-// function game(playerSelection) {
-//   let scores = { player: 0, computer: 0 };
-//   // for (let i = 0; i < 5; i++) {
-//   //   let playerSelection, result;
-//   //   console.log(`%cRound ${i + 1} :`,"font-weight: bold");
-//   // do {
-//   // playerSelection = prompt(`Round ${i + 1} : Rock / Paper / Scissors ?`);
-//   result = playRound(playerSelection, computerPlay());
-//   // if (result && !result.includes("draw")) scores[result] += 1;
-//   // } while (result === null);
-//   // }
-//   // console.log("%c--------------------------------------", "color: lightgray");
-//   // console.log("%cScore:", "font-weight: bold", scores);
-//   // console.log(
-//   //   "%cResult: ",
-//   //   "font-weight: bold",
-//   //   scores.player === scores.computer
-//   //     ? "Draw"
-//   //     : scores.player > scores.computer
-//   //     ? "You win!"
-//   //     : "You Lose"
-//   // );
-//   // console.log("%c--------------------------------------", "color: lightgray");
