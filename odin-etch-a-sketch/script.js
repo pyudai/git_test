fbd98742d3bd0c96ce908c2f08fad74208ab8bd4
pyudai <premyuda321@gmail.com>
2022-07-13 09:05:03 +0700

    add button for change squares per side

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,24 +1,46 @@
 function changeColor(e) {
-//   console.log(e.target);
-  e.target.classList.toggle('change');
-}
-
-const board = document.createElement("div");
-board.setAttribute("class", "board");
-
-const len = 16;
-
-for (let i = 0; i < len; i++) {
-  const cellCol = document.createElement("div");
-  cellCol.setAttribute("class", "cell-col");
-  for (let j = 0; j < len; j++) {
-    const cellRow = document.createElement("div");
-    cellRow.setAttribute("class", "cell-row");
-    cellRow.addEventListener("mouseover", changeColor);
-    cellCol.appendChild(cellRow);
-  }
-  board.appendChild(cellCol);
+  e.target.classList.toggle("change");
 }
 
 const body = document.querySelector("body");
-body.appendChild(board);
+let len = 16;
+
+function getLenTable(e) {
+  do {
+    len = prompt("How many squares per side do you want? (1-100)");
+    len = Number(len);
+    if (!len) alert("It's not a number. Please enter again.");
+    if(len <= 0 || len > 100)
+      alert("You should enter number between 1 to 100");
+  } while (!Number(len)||len <= 0 || len > 100);
+  createTable();
+}
+
+const btn = document.createElement("button");
+btn.addEventListener("click", getLenTable);
+btn.textContent = "Create new table";
+body.appendChild(btn);
+
+function createTable() {
+  document
+    .querySelectorAll(".board")
+    .forEach((board) => body.removeChild(board));
+
+  const board = document.createElement("div");
+  board.setAttribute("class", "board");
+
+  for (let i = 0; i < len; i++) {
+    const cellCol = document.createElement("div");
+    cellCol.setAttribute("class", "cell-col");
+    for (let j = 0; j < len; j++) {
+      const cellRow = document.createElement("div");
+      cellRow.setAttribute("class", "cell-row");
+      cellRow.addEventListener("mouseover", changeColor);
+      cellCol.appendChild(cellRow);
+    }
+    board.appendChild(cellCol);
+  }
+  body.appendChild(board);
+}
+
+createTable();
